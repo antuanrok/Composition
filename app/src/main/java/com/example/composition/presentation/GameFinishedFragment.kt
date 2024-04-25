19dc4dc31526acfb06ed05896cd9ffd386564164
antuanrok <a-gen@yandex.ru>
2024-04-25 16:19:29 +0300

    Подготовка ресурсов и макетов

---
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -0,0 +1,26 @@
+package com.example.composition.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.composition.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameFinishedFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameFinishedFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+    }
+
+}
